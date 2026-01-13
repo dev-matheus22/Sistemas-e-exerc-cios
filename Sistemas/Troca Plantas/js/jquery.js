bdbeb0dc1ac1af4f6006df105bbf954189394cc4
Matheus De Sousa <122496134+dev-matheus22@users.noreply.github.com>
2026-01-13 10:58:56 -0300

    Projetos relevantes

--- Sistemas/Troca Plantas/js/jquery.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    // Quando o item "Cadastro" for clicado
-    $("#cadastro").click(function(event){
-        $("#submenu").slideToggle(); // Mostra ou esconde o submenu com animação de deslizamento
-    });
-});
